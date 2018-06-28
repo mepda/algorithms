eb89f03ec2c3d7a0d9d8caf4f1741a7bf1feeb43
Ben S <bsadick@gmail.com>
2018-06-28 10:50:44 +0900

    first challenge reflections and tests

--- absoluteValuesSumMinization/absoluteValuesSumMinization.ts
+++ absoluteValuesSumMinization/absoluteValuesSumMinization.ts
@@ -5,7 +5,7 @@ function absoluteValuesSumMinimization(a: number[]): number {
   return a[Math.floor(a.length / 2)];
 }
 
-// console.log(absoluteValuesSumMinimization([2, 4, 7]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
-// console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
+console.log(absoluteValuesSumMinimization([2, 4, 7]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6]));
+console.log(absoluteValuesSumMinimization([2, 4, 7, 6, 6, 8]));
---
+++ notes.md
@@ -0,0 +1,5 @@
+#Thoughts about coding problems
+
+---
+
+1.  Try to use ternary operators more for more succinct code
