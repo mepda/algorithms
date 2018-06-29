2cc1a4216c3c46e7f64f0bef5396f4e7e0568a4c
Ben S <bsadick@gmail.com>
2018-06-29 21:47:41 +0900

    solution 4

--- addTwoDigits/addTwoDigits.ts
+++ addTwoDigits/addTwoDigits.ts
@@ -1,5 +1,6 @@
 function addTwoDigits(n: any): number {
-
+  return (n % 10) + Math.floor(n / 10)
 }
 
-// console.log(addTwoDigits(29));
+console.log(addTwoDigits(29));
+console.log(addTwoDigits(12));
