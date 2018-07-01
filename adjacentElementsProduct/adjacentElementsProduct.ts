0a08e9c55472c9edfafd8b343dc7eeaa46c76a9c
Ben S <bsadick@gmail.com>
2018-07-01 22:02:41 +0900

    fifth problem solved

--- adjacentElementsProduct/adjacentElementsProduct.ts
+++ adjacentElementsProduct/adjacentElementsProduct.ts
@@ -1,13 +1,20 @@
 function adjacentElementsProduct(inputArray: number[]): number {
-    let largestProduct = inputArray[0] * inputArray[1];
-
-    for (let i = 1; i < inputArray.length - 1; i++) {
-        const product = inputArray[i] * inputArray[i + 1];
-        
-        largestProduct = largestProduct < product ? product : largestProduct;
+  let firstProduct = inputArray[0] * inputArray[1];
+  for (let i = 1; i < inputArray.length - 1; i++) {
+    if (inputArray[i] * inputArray[i + 1] > firstProduct) {
+      firstProduct = inputArray[i] * inputArray[i + 1];
     }
+  }
+  return firstProduct;
+  // let largestProduct = inputArray[0] * inputArray[1];
 
-    return largestProduct;
+  // for (let i = 1; i < inputArray.length - 1; i++) {
+  //     const product = inputArray[i] * inputArray[i + 1];
+
+  //     largestProduct = largestProduct < product ? product : largestProduct;
+  // }
+
+  // return largestProduct;
 }
 
 console.log(adjacentElementsProduct([3, 6, -2, -5, 7, 3]));
